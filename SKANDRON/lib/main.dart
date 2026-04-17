03e5a99652282778b60b3077f281fec851bd6786
DimaCoder1 <147308935+DimaCoder1@users.noreply.github.com>
2026-04-17 22:34:08 +0300

    Реалізовуємо функціонал кнопок переходу на галерею і камеру. Якщо галерея, відкривається вікно з вибором  фото і тоді відкривається вікно з фото і кнопкою для сканування. Якщо кнопка камера, відкривається камера, робимо фото і тоді теж саме вікно відкривається для сканування

--- SKANDRON/lib/main.dart
+++ SKANDRON/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:math' as math;
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main()
 {
@@ -19,9 +21,134 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  File? _selectedImage;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> pickImage() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.gallery,
+      );
+
+      if (pickedFile != null) {
+        File image = File(pickedFile.path);
+
+        setState(() {
+          _selectedImage = image;
+        });
+
+        if (!mounted) return; 
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PreviewScreen(imageFile: image),
+          ),
+        );
+      }
+    } catch (e) {
+      print("Помилка при виборі фото: $e");
+    }
+  }
+
+  Future<void> takePhoto() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.camera,
+      );
+
+      if (pickedFile != null) {
+        File image = File(pickedFile.path);
+
+        if (!mounted) return;
+        
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PreviewScreen(imageFile: image),
+          ),
+        );
+      }
+    } catch (e) {
+      print("Помилка при використанні камери: $e");
+    }
+  }
+
+  void showUploadDialog(BuildContext context) {
+      showDialog(
+        context: context,
+        barrierColor: const Color(0xFFD9D9D9).withOpacity(0.6),
+        builder: (BuildContext context) {
+          return GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Material(
+                  color: Colors.transparent,
+                  child: Container(
+                    width: 360,
+                    height: 250,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF79AAD5).withOpacity(0.6),
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Оберіть спосіб завантажити фото',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 19,
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildDialogButton(
+                              imagePath: 'assets/images/GalleryBotton.svg', 
+                              label: 'ГАЛЕРЕЯ',
+                              onTap: () {
+                                Navigator.pop(context);
+                                pickImage();
+                              },
+                            ),
+                            buildDialogButton(
+                              imagePath: 'assets/images/CameraButton.svg', 
+                              label: 'КАМЕРА',
+                              onTap: () {
+                                Navigator.pop(context);
+                                takePhoto();
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,72 +275,6 @@ class MainScreen extends StatelessWidget {
       ),
     );
   }
-  void showUploadDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierColor: const Color(0xFFD9D9D9).withOpacity(0.6),
-      builder: (BuildContext context) {
-        return GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Center(
-            child: GestureDetector(
-              onTap: () {},
-              child: Material(
-                color: Colors.transparent,
-                child: Container(
-                  width: 360,
-                  height: 250,
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF79AAD5).withOpacity(0.6),
-                    border: Border.all(color: Colors.white, width: 2),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 20),
-                      const Text(
-                        'Оберіть спосіб завантажити фото',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Nunito',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 19,
-                          color: Colors.white,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildDialogButton(
-                            imagePath: 'assets/images/GalleryBotton.svg', 
-                            label: 'ГАЛЕРЕЯ',
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          buildDialogButton(
-                            imagePath: 'assets/images/CameraButton.svg', 
-                            label: 'КАМЕРА',
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
   Widget buildDialogButton({
     required String imagePath, 
     required String label, 
@@ -332,6 +393,74 @@ class ImageButton extends StatelessWidget {
                     fit: BoxFit.contain,
                   ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class PreviewScreen extends StatelessWidget {
+  final File imageFile;
+
+  const PreviewScreen({super.key, required this.imageFile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFDFECF2), 
+      appBar: AppBar(
+        title: const Text(
+          'Перегляд фото', 
+          style: TextStyle(
+            fontFamily: 'Nunito', 
+            color: Colors.white, 
+          ),
+        ),
+        backgroundColor: const Color(0xFF519CD0),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.file(imageFile, fit: BoxFit.contain),
+                ),
+              ),
+            ),
+            
+            ElevatedButton(
+              onPressed: () {
+                print("Запуск сканування...");
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF519CD0), 
+                foregroundColor: Colors.white,           
+                fixedSize: const Size(280, 60),          
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50), 
+                ),
+                elevation: 5,                            
+              ),
+              child: const Text(
+                "ЗАПУСТИТИ СКАНУВАННЯ",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ),
+            
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
